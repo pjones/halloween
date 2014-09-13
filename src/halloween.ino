d077e84b54acc146c9ccbeea0d74418172f075ed
Peter Jones <pjones@pmade.com>
2014-09-13 13:21:35 -0600

    Built a little state machine to make this more modular

--- src/halloween.ino
+++ src/halloween.ino
@@ -1,88 +1,190 @@
 // #############################################################################
-#define RANGE_PIN 3
-#define LED_R_PIN 9
-#define LED_G_PIN 10
-#define LED_B_PIN 11
+// The inactive status LED, color red pin.
+static const int PIN_STATUS_LED_RED = 9;
 
 // #############################################################################
-// Ardunio functions.
+// The inactive status LED, color green pin.
+static const int PIN_STATUS_LED_GREEN = 10;
+
+// #############################################################################
+// The inactive status LED, color blue pin.
+static const int PIN_STATUS_LED_BLUE = 11;
+
+// #############################################################################
+// The ultrasonic range finder.
+static const int PIN_RANGE_FINDER = 3;
+
+// #############################################################################
+// The solenoid which opens the air stream.
+static const int PIN_SOLENOID = 2;
+
+// #############################################################################
+// The secondary "within range" indicator LED.
+static const int PIN_RANGE_LED = 13;
+
+// #############################################################################
+// Length of time to spray air.
+static const unsigned long FIRE_TIME_MILLIS = 350;
+
+// #############################################################################
+// Length of time to remain inactive after spraying air.
+static const unsigned long INACTIVE_TIME_MILLIS = 30000;
+
+// #############################################################################
+// Distance in millimeters to trigger the air spray;
+static const unsigned long TRIGGER_DISTANCE = 1000;
+
+// #############################################################################
+enum states_t {
+    STATE_READY,
+    STATE_FIRING,
+    STATE_INACTIVE
+};
+
+// #############################################################################
+struct color_t {
+    int color_red;
+    int color_green;
+    int color_blue;
+};
+
+static const struct color_t COLOR_READY    = {0,   255,   0};
+static const struct color_t COLOR_FIRE     = {255, 0,     0};
+static const struct color_t COLOR_INACTIVE = {107, 38,  229};
+
+// #############################################################################
+class TriggerState {
+public:
+    TriggerState (void);
+    void check (void);
+    void start_fire (void);
+    void stop_fire (void);
+    void update_inactive_indicator (unsigned long duration);
+    void return_to_ready (void);
+    void indicate_distance (bool in_range);
+private:
+    unsigned long m_last_event;
+    enum states_t m_state;
+    struct color_t m_inactive_color;
+};
+
+TriggerState trigger_state_global; // Global state;
+
+// #############################################################################
+// Functions that the Ardunio preprocessor expects.
 void setup (void);
 void loop (void);
 
 // #############################################################################
 // Local functions.
-void state_ready (void);
-void state_fire (void);
-void state_pause (void);
-unsigned long sample_ranger_finder (void);
-void set_led_color (int red, int green, int blue);
+void set_led_color (struct color_t color);
 
 // #############################################################################
 // Run once.
 void setup()
 {
-    pinMode(RANGE_PIN, INPUT);
-    Serial.begin(9600);
+    pinMode(PIN_RANGE_FINDER, INPUT);
+
+    pinMode(PIN_SOLENOID, OUTPUT);
+    digitalWrite(PIN_SOLENOID, LOW);
+
+    pinMode(PIN_RANGE_LED, OUTPUT);
+    digitalWrite(PIN_RANGE_LED, LOW);
+
+//    Serial.begin(9600);
 }
 
 // #############################################################################
 // Run over and over.
 void loop()
 {
-    state_ready();
+    trigger_state_global.check();
+}
 
-    unsigned long range = 0;
-    range = sample_ranger_finder();
-    // Serial.print(range); Serial.println("");
+// #############################################################################
+void set_led_color (struct color_t color)
+{
+    analogWrite(PIN_STATUS_LED_RED,   color.color_red);
+    analogWrite(PIN_STATUS_LED_GREEN, color.color_green);
+    analogWrite(PIN_STATUS_LED_BLUE,  color.color_blue);
+}
 
-    if (range < 1000) {
-        state_fire();
-        state_pause();
+// #############################################################################
+TriggerState::TriggerState (void)
+    : m_last_event(0),
+      m_state(STATE_READY),
+      m_inactive_color(COLOR_INACTIVE)
+{
+    set_led_color(COLOR_READY);
+}
+
+// #############################################################################
+void TriggerState::check (void)
+{
+    unsigned long pulse = pulseIn(PIN_RANGE_FINDER, HIGH);
+    unsigned long now = millis();
+
+    indicate_distance(pulse <= TRIGGER_DISTANCE);
+
+    switch (m_state) {
+    case STATE_FIRING:
+        if (now - m_last_event >= FIRE_TIME_MILLIS) {
+            stop_fire();
+            update_inactive_indicator(0);
+            m_state = STATE_INACTIVE;
+            m_last_event = now;
+        }
+        break;
+    case STATE_INACTIVE:
+        update_inactive_indicator(now - m_last_event);
+
+        if (now - m_last_event >= INACTIVE_TIME_MILLIS) {
+            return_to_ready();
+            m_state = STATE_READY;
+        }
+        break;
+    case STATE_READY:
+        if (pulse <= TRIGGER_DISTANCE) {
+            start_fire();
+            m_state = STATE_FIRING;
+            m_last_event = now;
+        }
+        break;
+    }
+}
+
+// #############################################################################
+void TriggerState::start_fire (void)
+{
+    set_led_color(COLOR_FIRE);
+    digitalWrite(PIN_SOLENOID, HIGH);
+}
+
+// #############################################################################
+void TriggerState::stop_fire (void)
+{
+    digitalWrite(PIN_SOLENOID, LOW);
+}
+
+// #############################################################################
+void TriggerState::update_inactive_indicator (unsigned long duration) {
+    if (duration == 0) {
+        m_inactive_color = COLOR_INACTIVE;
+    } else if (INACTIVE_TIME_MILLIS / duration % 2 == 0) {
+        if (m_inactive_color.color_red   > 0) --m_inactive_color.color_red;
+        if (m_inactive_color.color_green > 0) --m_inactive_color.color_green;
+        if (m_inactive_color.color_blue  > 0) --m_inactive_color.color_blue;
     }
 
-    delay(10);
+    set_led_color(m_inactive_color);
 }
 
 // #############################################################################
-unsigned long sample_ranger_finder (void)
-{
-    unsigned long pulse = pulseIn(RANGE_PIN, HIGH);
-    return pulse;
+void TriggerState::return_to_ready (void) {
+    set_led_color(COLOR_READY);
 }
 
 // #############################################################################
-void state_ready (void)
-{
-    set_led_color(0, 100, 0); // Green.
-}
-
-// #############################################################################
-void state_fire (void)
-{
-    set_led_color(255, 0, 0); // Red.
-    delay(350);
-}
-
-// #############################################################################
-void state_pause (void)
-{
-    int delay_part=250;
-    int delay_total=30000;
-    int r=107, g=38, b=229; // Purple -> Blue;
-
-    for (int i=0; i<(delay_total/delay_part); ++i) {
-        set_led_color(r,g,b);
-        delay(delay_part);
-        if (r > 0) --r;
-        if (g > 0) --g;
-        if (b > 0) --b;
-    }
-}
-
-// #############################################################################
-void set_led_color (int red, int green, int blue)
-{
-    analogWrite(LED_R_PIN, red);
-    analogWrite(LED_G_PIN, green);
-    analogWrite(LED_B_PIN, blue);
+void TriggerState::indicate_distance (bool in_range) {
+    digitalWrite(PIN_RANGE_LED, in_range ? HIGH : LOW);
 }
